83cb2216383cce25e32c89294ad868cc3aa7c605
Caro Vazquez <caroyada@gmail.com>
2024-08-07 10:03:50 -0300

    Caro

---
+++ script.js
@@ -0,0 +1,17 @@
+function validarFomrulario(){
+    var nombreUsuario = document.getElementById("Usuario").value;
+    var contraseñaUsuario = document.getElementById("Contraseña").value;
+    var formulario = document.getElementById("Fomrulario");
+    var validador = true;
+    if (nombreUsuario.length>1) {
+        console.log("Nombre correcto");
+        document.getElementById("errorNombre").style="visibility:hidden";
+    } else {
+        validador = false;
+        document.getElementById("errorNombre").style="visibility:visible";
+    }
+    if (validador==true) {
+        formulario.submit();
+    }
+
+}
